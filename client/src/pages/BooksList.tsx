e2ee1f483c7e2bd0597caafe820762b5e5dbd84a
Leonardo Carreiro <leonardo.mcarreiro@gmail.com>
2021-11-03 22:45:11 -0400

    Add button to load more books

--- client/src/pages/BooksList.tsx
+++ client/src/pages/BooksList.tsx
@@ -1,25 +1,45 @@
+import { Button, Spin } from "antd";
 import React from "react";
 import { getBooks, Book } from "../api/book";
 import "./BooksList.scss";
 
 export const BooksList = () => {
+  const [loading, setLoading] = React.useState(false);
   const [pageToLoad, setPageToLoad] = React.useState(1);
   const [books, setBooks] = React.useState<Book[]>([]);
 
   React.useEffect(() => {
-    getBooks().then(newBooks => setBooks(existingBooks => [...existingBooks, ...newBooks]));
+    (async () => {
+      setLoading(true);
+
+      const newBooks = await getBooks(pageToLoad);
+
+      setBooks(existingBooks => [...existingBooks, ...newBooks]);
+      setLoading(false);
+    })();
   }, [pageToLoad]);
 
   return (
     <div className="books-list-page">
-      {books.map(book => (
-        <div key={book.id} className="book-item" onClick={() => {}}>
-          <div className="image">{book.image ? <img src={book.image} alt={book.title} /> : ""}</div>
-          <div className="title">
-            <h5>{book.title}</h5>
+      <div className="books-container">
+        {books.map(book => (
+          <div key={book.id} className="book-item" onClick={() => {}}>
+            <div className="image">{book.image ? <img src={book.image} alt={book.title} /> : ""}</div>
+            <div className="title">
+              <h5>{book.title}</h5>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div className="footer">
+        {loading ? (
+          <Spin />
+        ) : (
+          <Button type="primary" onClick={() => setPageToLoad(p => p + 1)}>
+            Load more books
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
