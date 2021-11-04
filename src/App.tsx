53fcc74ea3e821e6eecce7e0c109f1e059d63d3e
Leonardo Carreiro <leonardo.mcarreiro@gmail.com>
2021-11-03 20:53:15 -0400

    Install antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Layout } from "antd";
+import "antd/dist/antd.css";
+
+const { Header, Sider, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header>Header</Header>
+      <Layout>
+        <Sider>Sider</Sider>
+        <Content>Content</Content>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
   );
 }
 
